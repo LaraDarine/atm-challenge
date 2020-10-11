61a7658538b7e94d524d6dad26b4ce3bed438f12
LaraDarine <delara.risal2017@gmail.com>
2020-10-11 20:36:19 +0100

    Add blank methods :plus:

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ class ATM:
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name = bank_name
-        self.slices = ["100", "50", "10", "5"]
+        self.pieces = ["100", "50", "10", "5"]
 
     def withdraw(self, request):
         print("current balance =", self.balance, "your request =", request)
@@ -43,6 +43,13 @@ class ATM:
             result = self.balance - request
 
         return result
+    
+    def check_balance(self, request):
+        pass
+
+    def give_piece(self, request):
+        pass
+    
 
 
 balance1 = 500
