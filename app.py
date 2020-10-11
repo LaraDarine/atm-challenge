8e1155e683237a969026c9fa64af8c8170cfa7bb
LaraDarine <delara.risal2017@gmail.com>
2020-10-11 21:58:24 +0100

    Add uncleaned code :collision:

--- app.py
+++ app.py
@@ -3,64 +3,44 @@ class ATM:
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name = bank_name
-        self.pieces = ["100", "50", "10", "5"]
+        self.cache_categories = [100, 50, 10, 5, 1]
 
     def withdraw(self, request):
         print("current balance =", self.balance, "your request =", request)
         result = self.balance
+        self.check_balance(request)
 
+        return result
+
+    def check_balance(self, request):
         if request > self.balance:
-            print("Can't give you all this money !!")
+            print("Can't give you all this money!!")
 
         elif request < 0:
             print("More than zero plz!")
 
         else:
             self.balance -= request
+            self.give_cache_category(request)
 
-            while request > 0:
+        return self.balance
 
-                if request >= 100:
-                    request -= 100
-                    print("give 100")
+    def give_cache_category(self, request):
+        for cache_category in self.cache_categories:
+            while request >= cache_category:
+                request -= cache_category
+                print("give " + str(cache_category))
 
-                elif request >= 50:
-                    request -= 50
-                    print("give 50")
-
-                elif request >= 10:
-                    request -= 10
-                    print("give 10")
-
-                elif request >= 5:
-                    request -= 5
-                    print("give 5")
-
-                elif request >= 1:
-                    print("give 1")
-                    request = 0
-
-            result = self.balance - request
-
-        return result
-    
-    def check_balance(self, request):
-        pass
-
-    def give_piece(self, request):
-        pass
-    
+        return self.balance
 
 
 balance1 = 500
 balance2 = 1000
 
-
 atm1 = ATM(balance1, "Saba Bank")
-print("Welcome to Saba Bank")
+print("Welcome to " + atm1.bank_name)
 atm2 = ATM(balance2, "Jeen Bank")
-print("Welcome to Jeen Bank")
-
+print("Welcome to " + atm1.bank_name)
 
 atm1.withdraw(700)
 atm1.withdraw(421)
---
+++ not_cleaned.py
@@ -0,0 +1,62 @@
+class ATM:
+
+    def __init__(self, balance, bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
+
+    def withdraw(self, request):
+        print("current balance =" , self.balance, "your request =", request)
+        result = self.balance
+
+        if request > self.balance:
+            print("Can't give you all this money !!")
+
+        elif request < 0:
+            print("More than zero plz!")
+
+        else:
+            self.balance -= request
+
+            while request > 0:
+
+
+
+
+                if request >= 100:
+                    request -= 100
+                    print("give 100")
+
+                elif request >= 50:
+                    request -= 50
+                    print("give 50")
+
+                elif request >= 10:
+                    request -= 10
+                    print("give 10")
+
+                elif request >= 5:
+                    request -= 5
+                    print("give 5")
+
+                elif request >= 1:
+                    print("give 1")
+                    request = 0
+
+            result = self.balance - request
+
+        return result
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Saba Bank")
+print("Welcome to Saba Bank")
+atm2 = ATM(balance2, "Jeen Bank")
+print("Welcome to Jeen Bank")
+
+atm1.withdraw(700)
+atm1.withdraw(421)
+atm1.withdraw(82)
+
+atm2.withdraw(500)
+atm2.withdraw(450)
+atm2.withdraw(51)
