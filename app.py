d2eea26fd4414824587454f16a9c2ca1b7c2a9ca
LaraDarine <delara.risal2017@gmail.com>
2020-10-11 22:02:05 +0100

    print second atm welocoming :wave:

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class ATM:
         self.cache_categories = [100, 50, 10, 5, 1]
 
     def withdraw(self, request):
-        print("current balance =", self.balance, "your request =", request)
+        print("current balance = ", self.balance, "- your request = ", request)
         result = self.balance
         self.check_balance(request)
 
@@ -40,7 +40,7 @@ balance2 = 1000
 atm1 = ATM(balance1, "Saba Bank")
 print("Welcome to " + atm1.bank_name)
 atm2 = ATM(balance2, "Jeen Bank")
-print("Welcome to " + atm1.bank_name)
+print("Welcome to " + atm2.bank_name)
 
 atm1.withdraw(700)
 atm1.withdraw(421)
